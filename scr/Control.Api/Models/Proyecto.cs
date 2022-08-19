32ce0e9082ac01f2856994ba86419310bac3f364
Ing. German Alvarez <ayzcorp@hotmail.com>
2022-08-18 23:18:07 -0500

    TICKET#8 Creación de modelos.

---
+++ scr/Control.Api/Models/Proyecto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Control.Api.Models
+{
+    public partial class Proyecto
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string IdCiudad { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public string Tipo { get; set; } = null!;
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Observacion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? FormaContacto { get; set; }
+        public string? SistemaMedida { get; set; }
+        public string? IdentificacionResponsable { get; set; }
+        public string? NombreResponsable { get; set; }
+        public string? TelResponsable { get; set; }
+        public bool Activo { get; set; }
+        public byte Estado { get; set; }
+    }
+}
---
+++ scr/Control.Api/Models/VProyecto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Control.Api.Models
+{
+    public partial class VProyecto
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string Nombre1 { get; set; } = null!;
+    }
+}
