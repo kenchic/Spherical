2caf2e2c9f3ee2b7d33a78eb647e6de86804c4fe
Ing. German Alvarez <ayzcorp@hotmail.com>
2022-08-24 13:11:20 -0500

    TICKET#11 Actualización de modelos.

--- scr/Tactic.Api/Models/ListaPrecio.cs
+++ scr/Tactic.Api/Models/ListaPrecio.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 
 namespace Tactic.Api.Models
 {
-    public partial class Parametro
+    public partial class ListaPrecio
     {
-        public string Codigo { get; set; } = null!;
-        public string IdSistema { get; set; } = null!;
+        public byte Id { get; set; }
         /// <summary>
         /// CATALOGO=&gt;EMPRESA
         /// </summary>
         public string Empresa { get; set; } = null!;
         public string Nombre { get; set; } = null!;
-        public string? Descripcion { get; set; }
-        public string Valor { get; set; } = null!;
         public bool Activo { get; set; }
-
-        public virtual Sistema IdSistemaNavigation { get; set; } = null!;
     }
 }
--- scr/Tactic.Api/Models/Ticket.cs
+++ scr/Tactic.Api/Models/Ticket.cs
@@ -6,11 +6,18 @@ namespace Tactic.Api.Models
     public partial class Ticket
     {
         public int Id { get; set; }
+        /// <summary>
+        /// CATALOGO=&gt;EMPRESA
+        /// </summary>
+        public string Empresa { get; set; } = null!;
         public string Titulo { get; set; } = null!;
         public string Descripcion { get; set; } = null!;
         public string Tipo { get; set; } = null!;
         public string Prioridad { get; set; } = null!;
         public DateTime FechaCreacion { get; set; }
+        /// <summary>
+        /// CATALOGO=&gt;ESTADOS_TICKET
+        /// </summary>
         public string Estado { get; set; } = null!;
     }
 }
